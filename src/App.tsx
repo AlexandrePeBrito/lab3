35fa4a7b636b20142772dae814fd0e955ba79e00
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-12-11 07:15:06 -0300

    lab3

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Aplicativo Vazio</p>
+    </div>
+  );
+}
+
+export default App;
