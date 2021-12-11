7da8dbd14686f7fe72713b8e7e91b780c8b6b2f6
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-12-11 09:14:26 -0300

    Correto

--- tutorial1/src/App.tsx
+++ tutorial1/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import './cp/TitleApp'
+import TitleApp from './cp/TitleApp';
 
 function App() {
   return (
     <div className="App">
-      
+      <TitleApp></TitleApp>
     </div>
   );
 }
--- tutorial1/src/cp/TitleApp.tsx
+++ tutorial1/src/cp/TitleApp.tsx
@@ -1,10 +1,12 @@
 import {FC} from 'react';
 import '../App.css';
 
-const TitleApp:FC = () => {
+ const TitleApp:FC = () => {
     return(
         <div className='titleApp'>
             <h1>Aplicativo React+JS</h1>
         </div>
     )
 }
+
+export default TitleApp;
